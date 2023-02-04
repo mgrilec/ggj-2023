c0b4a94c53a7a4d05f717d1ccb2585363ba98406
Mihael Grilec <mihael.grilec@emilfreydigital.hr>
2023-02-04 19:59:41 +0100

    projectiles

--- Assets/Scripts/Abilities/InversePrimaryAbility.cs
+++ Assets/Scripts/Abilities/InversePrimaryAbility.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AcidPrimaryAbility : PlayerAbility
+public class InversePrimaryAbility : PlayerAbility
 {
 
 }
